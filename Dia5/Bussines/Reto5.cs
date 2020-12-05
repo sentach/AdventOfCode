eec3aca1ab15a564aae33a1fc9dedc68e9f06416
jiperez <jipcgr@gmail.com>
2020-12-05 16:09:51 +0100

    Solucion 1

---
+++ Dia5/Bussines/Reto5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bussines
+{
+    public class Reto5
+    {
+        public static int Resuelve1(List<string> datos)
+        {
+            var result = 0;
+            foreach(var dato in datos)
+            {
+                var id = CalculaId(dato);
+                result = Math.Max(id, result);
+            }
+            return result;
+        }
+
+        public static int CalculaId(string dato)
+        {
+            int row = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                if (dato[i] == 'B') 
+                {
+                    row += 1 << (6 - i);                    
+                }
+            }
+            int col = 0;
+            for (int j = 7; j < 10; j++)
+            {
+                if (dato[j] == 'R')
+                {
+                    col += 1 << (9 - j);
+                }
+            }
+            
+            return row * 8 + col;
+        }
+
+        public static int Resuelve2(List<string> datos)
+        {
+            return 0;
+            /*var result = 7;
+            foreach(dato in datos.OrderByDescending(x=>x.Replace("L","S"))
+            */
+        }
+    }
+}
---
+++ Dia5/Consola/Program.cs
@@ -0,0 +1,24 @@
+﻿using Bussines;
+using System;
+using System.Linq;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Calucula id {Reto5.CalculaId("FFFFFFFRRL")}");
+            var datos = Reader.Lector1(".\\datos.txt");
+            Console.WriteLine($"Calcula mas {Reto5.Resuelve1(datos)}");
+
+            
+            
+            foreach (var dato in datos.OrderByDescending(x => x.Replace("L","S")))
+            {
+                Console.WriteLine(dato);
+            }
+            
+        }
+    }
+}
